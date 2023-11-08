9f9d27bbff29952b0a2c79258791b34ce35b9c22
Dobios <adobis@student.ethz.ch>
2023-11-07 17:36:12 -0800

    setup npc scene and script

--- src/cs/core/Player.cs
+++ src/cs/core/Player.cs
@@ -27,114 +27,114 @@ using System;
 //      MagicHandler class which will become a child node of this class.
 public partial class Player : CharacterBody2D {
 
-    // ==================== Player specific enums ====================
-    // Models the different states the player may be in
-    private enum PlayerState {
-        IDLE, // The player isn't moving
-        WALKING, // The player is moving at a slow pace
-        RUNNING, // The player is moving at an accelerated pace
-        BLOCKED, // The player can't move (because of an active interaction or open menu)
-    };
+	// ==================== Player specific enums ====================
+	// Models the different states the player may be in
+	private enum PlayerState {
+		IDLE, // The player isn't moving
+		WALKING, // The player is moving at a slow pace
+		RUNNING, // The player is moving at an accelerated pace
+		BLOCKED, // The player can't move (because of an active interaction or open menu)
+	};
 
-    // ==================== Player Signals ====================
+	// ==================== Player Signals ====================
 
-    // ==================== Player Exports ====================
-    [ExportGroup("Movement Parameters")]
-    [Export]
-    // Describes how fast the player's run speed is
-    private int RunSpeed = 150;
+	// ==================== Player Exports ====================
+	[ExportGroup("Movement Parameters")]
+	[Export]
+	// Describes how fast the player's run speed is
+	private int RunSpeed = 150;
 
-    [Export]
-    // Describes how fast the player's walk speed is
-    private int WalkSpeed = 100;
+	[Export]
+	// Describes how fast the player's walk speed is
+	private int WalkSpeed = 100;
 
-    [Export]
-    // How quickly the player stops
-    private int Friction = 1000;
+	[Export]
+	// How quickly the player stops
+	private int Friction = 1000;
 
-    [Export]
-    // How quickly the player accelerates
-    private int Acceleration = 950;
+	[Export]
+	// How quickly the player accelerates
+	private int Acceleration = 950;
 
-    // ==================== Children Nodes ====================
-    // The player's sprite (i.e. visual element)
-    private Sprite2D Sprite;
+	// ==================== Children Nodes ====================
+	// The player's sprite (i.e. visual element)
+	private Sprite2D Sprite;
 
-    // The Animation player (used to activate certain animations)
-    private AnimationPlayer AP;
+	// The Animation player (used to activate certain animations)
+	private AnimationPlayer AP;
 
-    // The Animation Tree (used to create a set of transitions between animations)
-    private AnimationTree AT;
+	// The Animation Tree (used to create a set of transitions between animations)
+	private AnimationTree AT;
 
-    // The hitbox (used for collisions)
-    private CollisionShape2D Hitbox;
+	// The hitbox (used for collisions)
+	private CollisionShape2D Hitbox;
 
-    // Enables the use of input
-    private InputManager IM;
+	// Enables the use of input
+	private InputManager IM;
 
-    // Enables the use of interaction
-    private InteractionManager IntM;
+	// Enables the use of interaction
+	private InteractionManager IntM;
 
-    // ==================== Internal fields ====================
-    // Stores the players current state
-    private PlayerState State = PlayerState.IDLE;
+	// ==================== Internal fields ====================
+	// Stores the players current state
+	private PlayerState State = PlayerState.IDLE;
 
-    // ==================== GODOT Method Overrides ====================
-    // Called when the node enters the scene tree for the first time.
+	// ==================== GODOT Method Overrides ====================
+	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-        // Fetch the children nodes
-        Sprite = GetNode<Sprite2D>("Sprite");
-        AP = GetNode<AnimationPlayer>("AnimationPlayer");
-        AT = GetNode<AnimationTree>("AnimationTree");
-        Hitbox = GetNode<CollisionShape2D>("Hitbox");
-        IM = GetNode<InputManager>("InputManager");
-        IntM = GetNode<InteractionManager>("InteractionManager");
-    }
+		// Fetch the children nodes
+		Sprite = GetNode<Sprite2D>("Sprite");
+		AP = GetNode<AnimationPlayer>("AnimationPlayer");
+		AT = GetNode<AnimationTree>("AnimationTree");
+		Hitbox = GetNode<CollisionShape2D>("Hitbox");
+		IM = GetNode<InputManager>("InputManager");
+		IntM = GetNode<InteractionManager>("InteractionManager");
+	}
 
-    // Called at the start of every frame
-    public override void _Process(double delta){
-        base._Process(delta);
+	// Called at the start of every frame
+	public override void _Process(double delta) {
+		base._Process(delta);
 
-        // Handle the player's movement
-        HandleMovement(delta);
+		// Handle the player's movement
+		HandleMovement(delta);
 
-        // Handle the player's interactions
-        HandleInteraction();
-    }
+		// Handle the player's interactions
+		HandleInteraction();
+	}
 
-    // ==================== Internal Helpers ====================
-    // Handles the movement of the player
-    // Inputs are generally handled via polling on every frame
-    private void HandleMovement(double delta) {
-        // Retrieve the user's input
-        (Vector2 InputVec, bool RunRequest) = IM._GetInputVec(State != PlayerState.BLOCKED);
+	// ==================== Internal Helpers ====================
+	// Handles the movement of the player
+	// Inputs are generally handled via polling on every frame
+	private void HandleMovement(double delta) {
+		// Retrieve the user's input
+		(Vector2 InputVec, bool RunRequest) = IM._GetInputVec(State != PlayerState.BLOCKED);
 
-        // Figure out how fast we want our player to go
-        int Speed = RunRequest ? RunSpeed : WalkSpeed;
+		// Figure out how fast we want our player to go
+		int Speed = RunRequest ? RunSpeed : WalkSpeed;
 		
 		// Update the velocity based on the input vector
-        // If InputVec is Zero, then the player should stop moving
+		// If InputVec is Zero, then the player should stop moving
 		if(InputVec == Vector2.Zero) {
-            // Note that Velocity is a built-in field of the CharacterBody2D class
+			// Note that Velocity is a built-in field of the CharacterBody2D class
 			Velocity = Velocity.MoveToward(Vector2.Zero, (float)(Friction * delta));
 		} else {
-            // TODO: Set blend positions for animation tree
+			// TODO: Set blend positions for animation tree
 
-            // Update the movement velocity s.t. it moves towards the requested speed
-		    Velocity = Velocity.MoveToward(InputVec * Speed, (float)(Acceleration * delta));
+			// Update the movement velocity s.t. it moves towards the requested speed
+			Velocity = Velocity.MoveToward(InputVec * Speed, (float)(Acceleration * delta));
 		}
 
-        // Move the player using the built-in move&slide method for character bodies
-        MoveAndSlide();
-    }
+		// Move the player using the built-in move&slide method for character bodies
+		MoveAndSlide();
+	}
 
-    // Checks for an interaction input and calls the interaction handler
-    private void HandleInteraction() {
-        // Check the input manager for an interaction request
-        if(IM._CheckInteractionInput()) {
-            // Request an interaction
-            IntM._HandleInteraction();
-        }
-    }
+	// Checks for an interaction input and calls the interaction handler
+	private void HandleInteraction() {
+		// Check the input manager for an interaction request
+		if(IM._CheckInteractionInput()) {
+			// Request an interaction
+			IntM._HandleInteraction();
+		}
+	}
 }
 
